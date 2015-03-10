dd42b46b0351fd6fcb9791cc92b79da9d093f003
Tejas Mehta <tj@tejas-mehta.com>
2015-03-10 23:32:48 +0000

    Finished users signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      flash[:success] = "Welcome to the Jungle... only fun and games here!"
+      redirect_to @user
     else
       render 'new'
     end
