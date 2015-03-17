1296862de6dc7168875111fcffac17d770d0f032
Tejas Mehta <tj@tejas-mehta.com>
2015-03-17 00:14:48 +0000

    starting regression testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCRYPT::Engine.cost
+    BCrypt::Password.create(string, cost:cost)
+  end
 end
