73d3cf62068210ad98ef81c3bcde7ab97dd17bb4
Tejas Mehta <tj@tejas-mehta.com>
2015-03-26 00:30:16 +0000

    starting to be able to edit users

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
   
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: "",
@@ -16,4 +17,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                   }
     assert_template 'users/edit'
   end
+  
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Bob Hope"
+    email = "bob@akqa.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: "",
+                                    password_confirmation: ""
+                                  }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+  
 end
