1aa509a2ff25002c320b619e6452a42f27b00292
Tejas Mehta <tj@tejas-mehta.com>
2015-04-01 22:32:50 +0000

    account activation gtg

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,9 +35,19 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
   
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
   
   private
